6d54eb76564c370971d30afe8e8dfc88a11f8858
Hassan <hassan@gmail.com>
2026-08-06 13:34:34 +0600

    issue

--- src/components/TopGenerations.jsx
+++ src/components/TopGenerations.jsx
@@ -9,14 +9,8 @@ const TopGenerations = async() => {
 
 
     return (
-        <div>
-            <h1 className='text-sky-400 text-3xl my-5'>Top Photos</h1>
-            <div className='grid grid-cols-4 gap-5'>
-                {
-                    topPhotos.map(photo=><PhotoCard key={photo.id} photo={photo}></PhotoCard>)
-                }
-            </div>
-        </div>
+    
+    
     );
 };
 
