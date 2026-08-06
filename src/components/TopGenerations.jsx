a03e61e80db7388fc4b62bf217c38fbf3c34fdae
Hassan <hassan@gmail.com>
2026-08-06 13:46:21 +0600

    there is issue

--- src/components/TopGenerations.jsx
+++ src/components/TopGenerations.jsx
@@ -2,11 +2,6 @@ import { Key } from 'lucide-react';
 import React from 'react';
 import PhotoCard from './PhotoCard';
 
-const TopGenerations = async() => {
- const res =await fetch('http://localhost:3000/data.json')
- const photos=await res.json()
- const topPhotos=photos.slice(0,8)
-
 
     return (
         <div>
